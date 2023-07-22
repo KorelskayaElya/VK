4ba051403cc7e552b2fe380d8a29927fbc2e9f83
Эля Корельская <korelskaya2000@mail.ru>
2023-07-22 23:37:48 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Эля Корельская on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
