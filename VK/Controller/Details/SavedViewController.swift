769f0696e89d36a313d00b554c5ac28ac10d94e4
Эля Корельская <korelskaya2000@mail.ru>
2023-08-14 16:58:51 +0300

    add savedvc and filesvc

--- VK/Controller/Details/SavedViewController.swift
+++ VK/Controller/Details/SavedViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 
-class SavedViewController: UIViewController {
-
+class SavedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "SavedPostCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    var savedPosts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupView()
+    }
 
-        // Do any additional setup after loading the view.
+    private func setupView() {
+        self.view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+        ])
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedPosts.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SavedPostCell", for: indexPath) as? PostTableViewCell else {
+            fatalError("Could not dequeue a SavedPostTableViewCell")
+        }
+        
+        let post = savedPosts[indexPath.row]
+        //cell.configure(with: post.imagePost)
+        //cell.delegate = self
+        
+        return cell
     }
-    */
-
+    
+//    func postTableViewCellDidTapSave(_ cell: PostTableViewCell) {
+//        guard let indexPath = tableView.indexPath(for: cell) else { return }
+//        var post = likedPosts[indexPath.row]
+//        post.isLikedByCurrentUser.toggle()
+//        likedPosts[indexPath.row] = post
+//
+//        if post.isLikedByCurrentUser {
+//            likedPosts.remove(at: indexPath.row)
+//            savedPosts.append(post)
+//
+//        }
+//        tableView.reloadData()
+//    }
 }
+
